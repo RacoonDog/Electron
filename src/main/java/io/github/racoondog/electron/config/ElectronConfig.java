4b1594cf34d9eab5644edb605f57c75523b9f698
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2023-01-06 02:38:57 -0500

    use nio

--- src/main/java/io/github/racoondog/electron/config/ElectronConfig.java
+++ src/main/java/io/github/racoondog/electron/config/ElectronConfig.java
@@ -1,8 +1,13 @@
 package io.github.racoondog.electron.config;
 
+import io.github.racoondog.electron.Electron;
 import io.github.racoondog.electron.utils.Constants;
+import net.fabricmc.loader.api.FabricLoader;
 
-import java.io.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,24 +16,25 @@ public class ElectronConfig {
 
     static {
         try {
-            File file = Constants.CONFIG_FILE;
+            Path cfg = Constants.CONFIG_FILE;
             //Create if missing
-            if (!file.exists()) {
+            if (!Files.isRegularFile(cfg)) {
                 Constants.LOG.info("Generating config file...");
-                file.getParentFile().mkdirs();
-                file.createNewFile();
-                try (var writer = new FileWriter(file)) {
+                try (var writer = Files.newBufferedWriter(cfg, StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
                     writer.write("//See https://github.com/RacoonDog/Electron for a list of settings.\n");
                     writer.write("io.github.racoondog.electron.mixin.dev.profiler\n");
                     writer.write("io.github.racoondog.electron.mixin.starscript.section\n");
                 }
             }
-            //Read io.github.racoondog.electron.config
-            try (var reader = new BufferedReader(new FileReader(file))) {
+            //Read config
+            try (var reader = Files.newBufferedReader(cfg)) {
                 String line = reader.readLine();
 
                 while (line != null) {
-                    if (!line.startsWith("//") && !line.contains(".required")) ElectronConfig.SETTINGS.add(line);
+                    if (!line.startsWith("//") && !line.contains(".required")) {
+                        ElectronConfig.SETTINGS.add(line);
+                        if (FabricLoader.getInstance().isDevelopmentEnvironment()) Constants.LOG.info(line);
+                    }
                     line = reader.readLine();
                 }
             }
--- src/main/java/io/github/racoondog/electron/utils/Constants.java
+++ src/main/java/io/github/racoondog/electron/utils/Constants.java
@@ -6,13 +6,13 @@ import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
+import java.nio.file.Path;
 
 /**
  * Mixin safe objects
  */
 @Environment(EnvType.CLIENT)
 public final class Constants {
-    public static final File CONFIG_FILE = new File(new File(FabricLoader.getInstance().getGameDir().toString(), "meteor-client"), "electron.txt"); //Cannot use MeteorClient.FOLDER as it is not mixin safe
+    public static final Path CONFIG_FILE = FabricLoader.getInstance().getGameDir().resolve("meteor-client").resolve("electron.txt"); //Cannot use MeteorClient.FOLDER as it is not mixin safe
     public static final Logger LOG = LoggerFactory.getLogger("Electron");
 }
